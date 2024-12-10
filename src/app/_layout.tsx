1fb8b26de4890359289e3d51191d009b50d44ced
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-12-10 16:43:11 -0300

    feat: add react native bottom sheet dependency and its config

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,6 +2,7 @@ import { Loading } from '@/components/loading';
 import { colors } from '@/styles/theme';
 import { Rubik_400Regular, Rubik_500Medium, Rubik_600SemiBold, Rubik_700Bold, useFonts } from '@expo-google-fonts/rubik';
 import { Stack } from 'expo-router';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function Layout() {
 	const [fontsLoaded] = useFonts({
@@ -16,13 +17,15 @@ export default function Layout() {
 	}
 
 	return (
-		<Stack
-			screenOptions={{
-				headerShown: false,
-				contentStyle: {
-					backgroundColor: colors.gray[100]
-				}
-			}}
-		/>
+		<GestureHandlerRootView style={{ flex: 1 }}>
+			<Stack
+				screenOptions={{
+					headerShown: false,
+					contentStyle: {
+						backgroundColor: colors.gray[100]
+					}
+				}}
+			/>
+		</GestureHandlerRootView>
 	);
 }

f17b7ba03c2adac9aee085ca1f0a0de6deec27ec
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-12-10 11:26:35 -0300

    feat: add a very first version of the home page

---
+++ src/app/index.tsx
@@ -0,0 +1,16 @@
+import { Welcome } from '@/components/welcome';
+import { View } from 'react-native';
+
+export default function Index() {
+	return (
+		<View
+			style={{
+				flex: 1,
+				padding: 40,
+				gap: 40
+			}}
+		>
+			<Welcome />
+		</View>
+	);
+}

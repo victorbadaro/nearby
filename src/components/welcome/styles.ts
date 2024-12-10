e208ce33955da900c606258b8c18f0ed5aabc509
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-12-10 11:26:07 -0300

    feat: add welcome component

---
+++ src/components/welcome/styles.ts
@@ -0,0 +1,24 @@
+import { colors, fontFamily } from '@/styles/theme';
+import { StyleSheet } from 'react-native';
+
+export const s = StyleSheet.create({
+	logo: {
+		width: 48,
+		height: 48,
+		marginTop: 24,
+		marginBottom: 28
+	},
+
+	title: {
+		fontSize: 24,
+		fontFamily: fontFamily.bold,
+		color: colors.gray[600]
+	},
+
+	subTitle: {
+		fontSize: 16,
+		fontFamily: fontFamily.regular,
+		color: colors.gray[500],
+		marginTop: 12
+	}
+});
